f2567f08c3eb6afad6f8118b5b5eb0ac8484e2c2
OmkarHatti <omkarhatti23@gmail.com>
2026-05-11 17:55:06 +0530

    new one1

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       setLoading(true);
 
       const res = await axios.post(
-        "http://127.0.0.1:8000/upload",
+        "https://imagegeolocator-backend-2.onrender.com",
         formData,
         {
           headers: {
